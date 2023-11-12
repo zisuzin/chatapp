5b5ed36abca638d6db4ba7dd4230a9384e33170b
지수진 <zi_soo_zin@naver.com>
2023-11-12 23:27:34 +0900

    [추가] 환경변수 파일 작성

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+// CORS 허용
+const cors = this.require("cors");
+const app = express();
+app.use(cors());
+
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>console.log("connected to database"));
+
+module.exports = app;
