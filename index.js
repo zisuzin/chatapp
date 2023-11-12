62cd4f48689753b0d4623bf3ace818aec6c0dea2
지수진 <zi_soo_zin@naver.com>
2023-11-13 00:06:28 +0900

    [추가] socket.io-client 라이브러리 설치

---
+++ index.js
@@ -0,0 +1,15 @@
+const { createServer } = require("http");
+const app = require("./app");
+const {Server} = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+require("./utils/io")(io);
+httpServer.listen(process.env.PORT, () => {
+    console.log("server listening on port", process.env.PORT);
+});
